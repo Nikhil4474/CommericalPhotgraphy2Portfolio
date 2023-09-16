2480e1d5839f262a3695445f44793d13e988db21
Nikhil <ninuganti15@gmail.com>
2023-09-16 16:09:18 -0500

    added basic routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import Socials from './Socials';
+import Logo from '../img/header/logo.svg'
+import MobileNav from './MobileNav';
+import {Link} from 'react-router-dom';
+import Portfolio from '../pages/Portfolio'
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+      <div className="flex items-center justify-between w-full">
+        <Link to={'/'} className='mex-w-[200px]'>
+          <img
+            src={Logo}
+            alt=''
+          />
+        </Link>
+        <nav className="hidden lg:flex gap-x-12 font-semibold">
+          <Link
+            to={"/"}
+            className="text-[#696c6d hover:text-primary transition">
+            Home
+          </Link>
+          <Link
+            to={"/about"}
+            className="text-[#696c6d hover:text-primary transition">
+            About
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className="text-[#696c6d hover:text-primary transition">
+            Portfolio
+          </Link>
+          <Link
+            to={"/contact"}
+            className="text-[#696c6d hover:text-primary transition">
+            Contact
+          </Link>
+        </nav>
+      </div>
+        <Socials/>
+        <MobileNav/>
+    </header>
+  )
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      primary: 'Playfair Display',
-      secondary: 'Mulish',
+      primary: "Playfair Display",
+      secondary: "Mulish",
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1192px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1192px",
     },
     extend: {
       colors: {
-        primary: '#0E1112',
-        grey: '#484B4B',
+        primary: "#0E1112",
+        grey: "#484B4B",
+        "cambridge-blue": "#8DAA91",
+        "weird-purple": "#A9CB",
+        "rasin-black": "#272838",
+        "english-violet": "#5D576B",
+        "light-slate-grey": "#778DA9",
+        "pomp-and-power": "#7D6B91",
+        "sage": "#CCC9A1",
 
-        accent: '#EEF7F9',
+        accent: "#EEF7F9",
       },
     },
   },
   plugins: [],
-};
+}
