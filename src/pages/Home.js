6f6c183ea05339b1eeced5679d9989a809290dee
Nikhil <ninuganti15@gmail.com>
2023-09-20 22:55:42 -0500

    added transition effects to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
 import React from "react"
 import me from "../img/home/me.jpg"
 import { Link } from "react-router-dom"
+import { motion } from 'framer-motion'
+import { transition1} from '../transitions'
 
 const Home = () => {
   return (
-    <section className="section">
+    <motion.section 
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+      transition={transition1}
+    className="section">
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col justify-center">
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+          <motion.div 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            exit={{opacity:0}}
+            transition={transition1}
+          className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
             <h1 className="h1">
               photographer <br /> & film maker
             </h1>
@@ -19,7 +31,7 @@ const Home = () => {
               className="btn mb-[30px]">
               Hire Me
             </Link>
-          </div>
+          </motion.div>
           <div className="flex justify-end max-h-96 lg:max-h-max">
             <div className="relative lg:-right-40 overflow-hidden">
               <img
@@ -29,7 +41,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
