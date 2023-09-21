2a2473db922b7ecae6b764634349bf23991071c7
Nikhil <ninuganti15@gmail.com>
2023-09-20 22:18:01 -0500

    finished contact page view

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,17 @@
 import React from "react"
-import me from "../img/contact/me.jpeg"
+import me from "../img/contact/fancyme.png"
 
 const Contact = () => {
   return (
     <section className="section bg-white">
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left">
-          <div>bg</div>
-          <div className="lg:flex lg:pt-32 px-4">
+          <div className='hidden lf:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10'>bg</div>
+          <div className="lg:flex-l lg:pt-32 px-4">
             <h1 className="h1">Contact me</h1>
             <p className="">I would love to get suggestions from you.</p>
-            <form className="">
-              <div className='flex gap-x-10'>
+            <form className="flex flex-col gap-y-4">
+              <div className="flex gap-x-10">
                 <input
                   className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879"
                   type="text"
@@ -23,9 +23,17 @@ const Contact = () => {
                   placeholder="Your email address"
                 />
               </div>
+                <input
+                  className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879"
+                  type="text"
+                  placeholder="Your message"
+                />
+              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">Send It</button>
             </form>
           </div>
-          <div>image</div>
+            <div className='lg:flex-1'>
+              <img src={me} alt=''/>
+            </div>
         </div>
       </div>
     </section>
